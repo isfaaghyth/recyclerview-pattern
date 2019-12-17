82d74b20a2342b6fbbcce78d51baa2356aba0f36
isfaaghyth <isfaaghyth@gmail.com>
2019-12-18 00:00:43 +0700

    added overview menu item and showing into list item

--- app/src/main/java/app/isfaaghyth/visitablelist/MainActivity.kt
+++ app/src/main/java/app/isfaaghyth/visitablelist/MainActivity.kt
@@ -2,12 +2,39 @@ package app.isfaaghyth.visitablelist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import app.isfaaghyth.visitablelist.base.BaseListAdapter
+import app.isfaaghyth.visitablelist.entity.Menu
+import app.isfaaghyth.visitablelist.entity.OverviewMenu
+import app.isfaaghyth.visitablelist.factory.ItemTypeFactoryImpl
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: BaseListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = BaseListAdapter(ItemTypeFactoryImpl())
+        lstItems.layoutManager = LinearLayoutManager(this)
+        lstItems.adapter = adapter
+
+        overviewMenu()
+    }
+
+    private fun overviewMenu() {
+        val overviewMenus = listOf(
+            OverviewMenu(
+                Menu(R.mipmap.ic_overview_scan, "Pindai"),
+                Menu(R.mipmap.ic_overview_pay, "Bayar"),
+                Menu(R.mipmap.ic_overview_history, "Riwayat"),
+                Menu(R.mipmap.ic_overview_user, "Akun")
+            )
+        )
+
+        adapter.addItem(overviewMenus)
     }
 
 }
