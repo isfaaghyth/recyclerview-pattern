21b26568500e90ba346b4a20dd1c0cc92100d084
isfaaghyth <isfaaghyth@gmail.com>
2019-12-17 23:58:37 +0700

    created overview menu (menu wrapper) and attach it onto item factory

--- app/src/main/java/app/isfaaghyth/visitablelist/factory/ItemTypeFactory.kt
+++ app/src/main/java/app/isfaaghyth/visitablelist/factory/ItemTypeFactory.kt
@@ -2,7 +2,10 @@ package app.isfaaghyth.visitablelist.factory
 
 import android.view.View
 import app.isfaaghyth.visitablelist.base.AbstractViewHolder
+import app.isfaaghyth.visitablelist.entity.OverviewMenu
 
 interface ItemTypeFactory {
+    fun type(typeFactory: OverviewMenu): Int
+
     fun createViewHolder(parent: View, type: Int): AbstractViewHolder<*>
 }
